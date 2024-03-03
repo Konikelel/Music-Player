a91bc2d2ba165b3636af72589e29789c4ebfb123
Konikelel <wiktoralbinowski@gmail.com>
2024-03-03 19:17:12 +0100

    add Song component

---
+++ src/components/Song.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Song() {
+    return (
+        <div className='song'>
+            <p className='picture'>Picture</p>
+            <h2 className='title'>Title</h2>
+            <h3 className='author'>Author</h3>
+        </div>
+    );
+}
+
+export default Song;
