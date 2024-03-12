77eb8bc31c4d1ab1b9be479a53fb2c30cc4d1627
Konikelel <wiktoralbinowski@gmail.com>
2024-03-12 14:54:16 +0100

    add cursor pointer to nav button, move onClick event to button

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,12 +2,15 @@ import { faMusic } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-function Nav() {
+function Nav({ showLibrary, setShowLibrary }) {
     return (
         <div className='nav'>
             <nav>
                 <h1 className='title'>Waves</h1>
-                <button className='button'>
+                <button
+                    className='button'
+                    onClick={() => setShowLibrary(!showLibrary)}
+                >
                     <div className='buttonContainer'>
                         <p>Library</p>
                         <FontAwesomeIcon className='music' icon={faMusic} />
